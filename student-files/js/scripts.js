6d1e994f66ff688898a26cbec4b6aea6513ceed8
DevinRamirez1 <devinramirez207@gmail.com>
2021-09-14 14:55:22 -0500

    moved js to student files

---
+++ student-files/js/scripts.js
@@ -0,0 +1,44 @@
+
+//fetch user data fom url
+function fetchUserData(url) {
+    return fetch(url)
+        .then(checkStatus)
+        .then(response => response.json())
+        .catch(error => response.json())
+        .then(data => console.log(data))
+}
+Promise.all([
+    fetchUserData('https://randomuser.me/api/?results=5000')
+])
+
+
+//Generate each User's gallery card
+function generateUsersHTML(data) {
+    data.map( person => {
+        const gallery = document.getElementById('gallery');
+        if (person.type == 'standard') {
+            gallery.innerHTML = 
+            `<div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">first last</h3>
+                <p class="card-text">email</p>
+                <p class="card-text cap">city, state</p>
+            </div>
+        </div>`
+        }
+    })
+}
+
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+
+
+
