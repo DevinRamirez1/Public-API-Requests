ec298c3166161cb0039c96b4b11069ba363a6a11
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-09-15 17:49:54 -0500

    Created a modal box
    Created a modal box that displays the users info when card is clicked

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -1,6 +1,9 @@
 //variables
 const gallery = document.getElementById('gallery');
 const userURL = 'https://randomuser.me/api/?results=12';
+let currentIndex = 0;
+let users = []
+let data
 
 //fetch user data fom url
 fetch(userURL)
@@ -14,7 +17,8 @@ fetch(userURL)
 
 //Generate each User's gallery card
 function generateUsersHTML(data) {
-    data.forEach( (user, index) => {
+    users = data;
+    users.forEach( (user, index) => {
         gallery.insertAdjacentHTML('beforeend', 
         `<div class="card" data-index=${index}>
             <div class="card-img-container">
@@ -40,4 +44,39 @@ function checkStatus(response) {
     }
 }
 
+//generate modal box html
+function generateModalBoxHTML(index) {
+    const {name, email, location, phone, dob, picture} = users[index];
+    
+    gallery.insertAdjacentHTML("beforeend", 
+        `<div class="modal-container">
+                 <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${picture.large}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+                        <p class="modal-text">${email}</p>
+                        <p class="modal-text cap">${location.city}</p>
+                        <hr>
+                        <p class="modal-text">${phone}</p>
+                        <p class="modal-text">${location.street}, ${location.city}, ${location.state} ${location.postcode}</p>
+                        <p class="modal-text">Birthday: ${dob}</p>
+                    </div>
+                </div>
 
+                // IMPORTANT: Below is only for exceeds tasks 
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>`);
+
+}
+
+gallery.addEventListener('click', (e) => {
+    const card = e.target.closest('.card');
+    const index = card.getAttribute('data-index');
+    currentIndex = index
+    generateModalBoxHTML(currentIndex);
+
+})
